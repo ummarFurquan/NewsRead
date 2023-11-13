5d111fe1b3c6e7dc12ee01738a42147556171886
ummarFurquan <mohammedummar29@gmail.com>
2023-11-13 19:09:25 +0530

    Changed to function based component

--- src/App.js
+++ src/App.js
@@ -1,69 +1,42 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import Navbar from './components/Navbar';
 import News from './components/News';
 import About from './components/about'; 
 import Weather from './components/weather'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import LoadingBar from 'react-top-loading-bar';
-export default class App extends Component {
-  pageSize = 15;
 
-    state = {
-      progress: 0
-    }
-  
+const App = () => {
+  const pageSize = 15;
+  const [progress, setProgress] = useState(0);
 
-  setProgress = (progress) =>  {
-    this.setState({ progress: progress })
-    }
-  
-  render() {
+  return (
+    <>
+      <div>
+        <Router>
+          <Navbar />
 
-    return (
-      <>
-        <div>
-          <Router>
-            <Navbar />
+          <LoadingBar
+            color="#FF0000"
+            height={4}
+            progress={progress}
+          />
+          <Routes>
+            <Route path="/" element={<News setProgress={setProgress} key="general" pageSize={pageSize} country="in" category="General" />} />
+            <Route path="/Business" element={<News setProgress={setProgress} key="business" pageSize={pageSize} country="in" category="Business" />} />
+            <Route path="/Entertainment" element={<News setProgress={setProgress} key="entertainment" pageSize={pageSize} country="in" category="Entertainment" />} />
+            <Route path="/General" element={<News setProgress={setProgress} key="general" pageSize={pageSize} country="in" category="General" />} />
+            <Route path="/Health" element={<News setProgress={setProgress} key="health" pageSize={pageSize} country="in" category="Health" />} />
+            <Route path="/Science" element={<News setProgress={setProgress} key="science" pageSize={pageSize} country="in" category="Science" />} />
+            <Route path="/Sports" element={<News setProgress={setProgress} key="sports" pageSize={pageSize} country="in" category="Sports" />} />
+            <Route path="/Technology" element={<News setProgress={setProgress} key="technology" pageSize={pageSize} country="in" category="Technology" />} />
+            <Route path="/weather" element={<Weather setProgress={setProgress} />} />
+            <Route path="/about" element={<About />} />
+          </Routes>
+        </Router>
+      </div>
+    </>
+  );
+};
 
-
-
-            <LoadingBar
-             color="#FF0000"
-             height={4}
-            progress={this.state.progress} />
-            <Routes>
-
-              <Route path="/" element={<News setProgress={this.setProgress}  key="general" pageSize={this.pageSize} country="in" category="General"/>}></Route>
-
-              <Route path="/Business" element={<News setProgress={this.setProgress}  key="business" pageSize={this.pageSize} country="in" category="Business"/>}></Route>
-
-              <Route path="/Entertainment" element={<News setProgress={this.setProgress}  key="entertainment"pageSize={this.pageSize}country="in"category="Entertainment"/>}></Route>
-
-              <Route path="/General" element={<News setProgress={this.setProgress}  key="general" pageSize={this.pageSize} country="in" category="General"/>}></Route>
-
-              <Route path="/Health" element={<News setProgress={this.setProgress}  key="health" pageSize={this.pageSize} country="in" category="Health"/>}></Route>
-
-              <Route path="/Science" element={<News setProgress={this.setProgress}  key="science" pageSize={this.pageSize} country="in" category="Science"/>}></Route>
-
-              <Route path="/Sports" element={<News setProgress={this.setProgress}  key="sports" pageSize={this.pageSize} country="in" category="Sports"/>}></Route>
-
-              <Route path="/Technology" element={<News setProgress={this.setProgress}  key="technology" pageSize={this.pageSize} country="in" category="Technology"/>}></Route>
-              
-              <Route path="/weather" element={<Weather setProgress={this.setProgress}/>}></Route>
-              
-              <Route path="/about" element={<About/>}></Route>
-
-
-
-            </Routes>
-
-          </Router>
-           
-        </div>
-
-      
-      </>
-    );
-  }
-}
-
+export default App;
