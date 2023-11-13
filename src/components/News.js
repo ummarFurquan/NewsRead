6c1747572dbf6cc4eccd65e7972e56396d36c914
ummarFurquan <mohammedummar29@gmail.com>
2023-11-13 23:37:05 +0530

    Changed to function based component

--- src/components/News.js
+++ src/components/News.js
@@ -1,105 +1,120 @@
-import React, { Component } from 'react'
-import NewsItem from './NewsItem'
-import PropTypes from 'prop-types'
-import ScrollToTop from 'react-scroll-to-top'
+import React, { useState, useEffect } from 'react';
+import NewsItem from './NewsItem';
+import PropTypes from 'prop-types';
+import ScrollToTop from 'react-scroll-to-top';
 
-export  class News extends Component {
-    
+const News = ({ country = 'in', pageSize = 8, category = 'general', setProgress }) => {
+  const [articles, setArticles] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [page, setPage] = useState(1);
+  const [totalResults, setTotalResults] = useState(0);
+  useEffect(() => {
+    const fetchData = async () => {
+      setProgress(10);
+      const url = `https://newsapi.org/v2/top-headlines?country=${country}&category=${category}&apiKey=258548f6da1a432fa4a81a624f5623b5&page=1&pageSize=${pageSize}`;
+      setLoading(true);
+      try {
+        const data = await fetch(url);
+        const parsedData = await data.json();
+        setProgress(50);
+        setArticles(parsedData.articles);
+        setTotalResults(parsedData.totalResults);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+      setLoading(false);
+      setProgress(100);
+    };
 
-    static defaultProps =  {
-        country : "in",
-        pageSize : 8,
-        category : 'general'
+    fetchData();
+  }, [country, category, pageSize, setProgress]);
+
+  const nextClick = async () => {
+    setProgress(10);
+    if (!(page + 1 > Math.ceil(totalResults / pageSize))) {
+      window.scrollTo(0, 0);
+      const url = `https://newsapi.org/v2/top-headlines?country=${country}&category=${category}&apiKey=258548f6da1a432fa4a81a624f5623b5&page=${page + 1}&pageSize=${pageSize}`;
+      setLoading(true);
+      try {
+        const data = await fetch(url);
+        const parsedData = await data.json();
+        setProgress(50);
+        setPage(page + 1);
+        setArticles(parsedData.articles);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+      setLoading(false);
+      setProgress(100);
     }
-    static propTypes = {
-        country : PropTypes.string,
-        pageSize: PropTypes.number,
-        category : PropTypes.string
+  };
 
+  const prevClick = async () => {
+    setProgress(0);
+    window.scrollTo(0, 0);
+    const url = `https://newsapi.org/v2/top-headlines?country=${country}&category=${category}&apiKey=258548f6da1a432fa4a81a624f5623b5&page=${page - 1}&pageSize=${pageSize}`;
+    setLoading(true);
+    try {
+      const data = await fetch(url);
+      const parsedData = await data.json();
+      setProgress(50);
+      setPage(page - 1);
+      setArticles(parsedData.articles);
+    } catch (error) {
+      console.error('Error fetching data:', error);
     }
-    constructor(){
-        super();
-        this.state = {
-            articles : [],
-            loading : false,
-            page : 1
-        }
+    setLoading(false);
+    setProgress(100);
+  };
 
-    }
-
-  
-    async componentDidMount(){
-        this.props.setProgress(10)
-            let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=258548f6da1a432fa4a81a624f5623b5&page=1&pageSize=${this.props.pageSize}`;
-            let data = await fetch(url);
-            this.setState({loading : true});
-            let parsedData = await data.json();
-            this.props.setProgress(50)
-            this.setState({articles: parsedData.articles, 
-                totalResults : parsedData.totalResults,
-                loading : false
-            })
-            this.props.setProgress(100)
-    }
-
-    
-
-    nextClick = async () => {
-        this.props.setProgress(10)
-        if (!(this.state.page + 1 > Math.ceil(this.state.totalResults/this.props.pageSize))){
-        window.scrollTo(0,0)
-        let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=258548f6da1a432fa4a81a624f5623b5&page=${this.state.page + 1}&pageSize=${this.props.pageSize}`;
-        this.setState({loading : true});
-        let data = await fetch(url);
-        let parsedData = await data.json();
-        this.props.setProgress(50)
-        this.setState({page : this.state.page + 1,
-            articles: parsedData.articles,
-            loading : false
-        })
-    }
-    this.props.setProgress(100)
-    }
-    
- 
-
-
-    prevClick = async () => {
-        this.props.setProgress(0)
-        window.scrollTo(0,0)
-        let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=258548f6da1a432fa4a81a624f5623b5&page=${this.state.page - 1}&pageSize=${this.props.pageSize}`;
-        let data = await fetch(url);
-        this.setState({loading : true});
-        let parsedData = await data.json();
-        this.props.setProgress(50)
-        this.setState({
-            page : this.state.page - 1,
-            articles: parsedData.articles,
-            loading : false
-        })
-        this.props.setProgress(100)
-    }
-    
-
-  render() {
-    return (
-        <div className="container my-3" >
-        <h1 className='text-center' style={{margin:'80px'}}>Top Headlines - <span style={{color:'red'}}>{this.props.category}</span></h1> 
-        <div className="row">
-        {this.state.articles.map((element) => {
-            return <div className="col-md-4" key={element.url}>
-        <NewsItem title={element.title ? element.title.slice(0,50):""} description={element.description ? element.description.slice(0,100):""} imageUrl={element.urlToImage ? element.urlToImage:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQVKQFZYhRFKtlmdWAH1PBN9kBmLmQl2olzAtXd35KpIRqZ6bFv2KHuMBAqyVmzdUrFZJA&usqp=CAU"} 
-        newsUrl = {element.url} author= {element.author ? element.author:"Unknown"} publishedOn={element.publishedAt ? element.publishedAt:"Not Specified"} source = {element.source.name}/>
-        </div> 
-        })} 
-        </div>
-        <div className="container d-flex justify-content-between my-4">
-            <button disabled={this.state.page <= 1}className='btn btn-dark' onClick={this.prevClick}> &laquo; Previous</button>
-            <button disabled={(this.state.page + 1 > Math.ceil(this.state.totalResults/this.props.pageSize))}    className='btn btn-dark' onClick={this.nextClick}>Next &raquo;</button>
-        </div>
-        <ScrollToTop smooth color='#FF0000' width='80px' height='30px' title='Scroll To Top'/>
+  return (
+    <div className="container my-3">
+      <h1 className="text-center" style={{ margin: '80px' }}>
+        Top Headlines - <span style={{ color: 'red' }}>{category}</span>
+      </h1>
+      <div className="row">
+        {articles.map((element) => (
+          <div className="col-md-4" key={element.url}>
+            <NewsItem
+              title={element.title ? element.title.slice(0, 50) : ''}
+              description={element.description ? element.description.slice(0, 100) : ''}
+              imageUrl={element.urlToImage ? element.urlToImage : 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQVKQFZYhRFKtlmdWAH1PBN9kBmLmQl2olzAtXd35KpIRqZ6bFv2KHuMBAqyVmzdUrFZJA&usqp=CAU'}
+              newsUrl={element.url}
+              author={element.author ? element.author : 'Unknown'}
+              publishedOn={element.publishedAt ? element.publishedAt : 'Not Specified'}
+              source={element.source.name}
+            />
+          </div>
+        ))}
       </div>
-    )
-  }
-}
+      <div className="container d-flex justify-content-between my-4">
+        <button disabled={page <= 1} className="btn btn-dark" onClick={prevClick}>
+          &laquo; Previous
+        </button>
+        <button
+          disabled={page + 1 > Math.ceil(totalResults / pageSize)}
+          className="btn btn-dark"
+          onClick={nextClick}
+        >
+          Next &raquo;
+        </button>
+      </div>
+      <ScrollToTop smooth color="#FF0000" width="80px" height="30px" title="Scroll To Top" />
+    </div>
+  );
+};
+
+News.defaultProps = {
+  country: 'in',
+  pageSize: 8,
+  category: 'general',
+};
+
+News.propTypes = {
+  country: PropTypes.string,
+  pageSize: PropTypes.number,
+  category: PropTypes.string,
+  setProgress: PropTypes.func.isRequired,
+};
+
 export default News
